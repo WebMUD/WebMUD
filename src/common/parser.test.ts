9ba3db55dcf98cc62b6ad2448b1a8f466686cc2b
chizeks <sarachizek@gmail.com>
2022-10-21 14:00:21 -0400

    help

---
+++ src/common/parser.test.ts
@@ -0,0 +1,13 @@
+import * as util from '../common/util';
+import * as Parser from './parser';
+
+
+function evaluate(input : string):any 
+{
+    const p = new Parser.Parser(input);
+    
+    
+}
+
+evaluate("Move south");
+evaluate("Move North");
