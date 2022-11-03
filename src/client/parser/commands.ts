6af47dfa0411ad51e5d82828dd47e41520b1c208
Andrew Johnson <ajzat25@gmail.com>
2022-11-03 13:26:25 -0400

    parser setup

---
+++ src/client/parser/commands.ts
@@ -0,0 +1,14 @@
+export enum Direction {
+    NORTH = 'NORTH',
+    SOUTH = 'SOUTH',
+    EAST = 'EAST',
+    WEST = 'WEST',
+}
+
+export class MoveCommand {
+    direction: Direction;
+
+    constructor(direction: Direction) {
+        this.direction = direction;
+    }
+}
