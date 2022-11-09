817b7d5f5ab8146aa4653e106b8d59cc3f8cbff4
Andrew Johnson <ajzat25@gmail.com>
2022-11-09 15:54:27 -0500

    change data-attribute classes

--- src/server/gamestate/components/base/event-channel.ts
+++ src/server/gamestate/components/base/event-channel.ts
@@ -1,8 +1,8 @@
 import { EventEmitter } from '../../../../common/event-emitter';
 import { Component } from './component';
 
-export class EventChannelComponent extends Component {
-  event = EventEmitter.channel<void>();
+export class EventChannelComponent<T> extends Component {
+  event = EventEmitter.channel<T>();
 
   constructor() {
     super();
