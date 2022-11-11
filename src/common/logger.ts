d77239a6562badfc2117a6ce5336d854112a1f61
Andrew Johnson <ajzat25@gmail.com>
2022-11-11 13:16:38 -0500

    add clear

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -2,11 +2,25 @@ import { EventEmitter } from "./event-emitter";
 import { View } from "./view";
 
 export class Logger {
+    // command
+    public onClear = EventEmitter.channel<void>();
+
+    // out
     public onPrint = EventEmitter.channel<string[]>();
     public onDebug = EventEmitter.channel<string[]>();
     public onInfo = EventEmitter.channel<string[]>();
     public onWarning = EventEmitter.channel<string[]>();
     public onError = EventEmitter.channel<string[]>();
+    public onBold = EventEmitter.channel<string[]>();
+    public onSmall = EventEmitter.channel<string[]>();
+
+    // in
+    public onInput = EventEmitter.channel<string>();
+
+    
+    public clear() {
+        this.onClear.emit();
+    }
 
     public print(...data: string[]) {
         this.onPrint.emit(data);
@@ -28,19 +42,43 @@ export class Logger {
         this.onError.emit(data);
     }
 
+    public bold(...data: string[]) {
+        this.onBold.emit(data);
+    }
+
+    public small(...data: string[]) {
+        this.onSmall.emit(data);
+    }
+    
+    public input(data: string) {
+        this.onInput.emit(data);
+    }
+
     useConsole() {
+        this.onClear(_=>console.log());
+
         this.onPrint(data=>console.log(...data));
         this.onDebug(data=>console.debug(...data));
         this.onInfo(data=>console.info(...data));
         this.onWarning(data=>console.warn(...data));
         this.onError(data=>console.error(...data));
+
+        this.onBold(data=>console.log(...data));
+        this.onSmall(data=>console.log(...data));
     }
 
     useView(view: View) {
+        this.onClear(_=>view.clear());
+
         this.onPrint(data=>view.print(...data));
         this.onDebug(data=>view.debug(...data));
         this.onInfo(data=>view.info(...data));
         this.onWarning(data=>view.warn(...data));
         this.onError(data=>view.error(...data));
+
+        this.onBold((data)=>view.print(view.formatBold(data.join(' '))));
+        this.onSmall((data)=>view.print(view.formatSmall(data.join(' '))));
+
+        view.onInput((data)=>this.input(data));
     }
 }
