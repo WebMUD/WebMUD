790ab20b7e90ca7c9019544a6ffa79ab34ba8f61
Andrew Johnson <ajzat25@gmail.com>
2022-11-13 19:29:01 -0500

    added .emitter to eventchannel

--- src/common/event-emitter.ts
+++ src/common/event-emitter.ts
@@ -15,6 +15,8 @@ export interface EventChannel<T> {
   clear: () => void;
   /** listen to one event only */
   once: (cb: (data: T) => void) => void;
+  /** the underlaying EventEmitter object */
+  emitter: EventEmitter<T>;
 }
 
 /**
@@ -51,7 +53,8 @@ export class EventEmitter<T> {
    * @param data data to call handlers with
    */
   public emit(data: T): void {
-    for (const handler of this.handlers) handler(data);
+    const _handelrs = [...this.handlers];
+    for (const handler of _handelrs) handler(data);
   }
 
   /**
@@ -72,6 +75,7 @@ export class EventEmitter<T> {
         stop();
       });
     };
+    result.emitter = eventEmitter;
     return result;
   }
 }
