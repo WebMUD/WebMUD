f13c751547ac1e3fa7aa7e31af5c1c23d8c49afe
Marley <Marley@MacBook-Pro.local>
2022-11-16 21:22:43 -0500

    added greeter

--- src/server/plugins/npc-greeter-plugin.ts
+++ src/server/plugins/npc-greeter-plugin.ts
@@ -1,12 +1,30 @@
+import { add } from "lodash";
+import { ChatChannel, HierarchyChild, HierarchyContainer, Name, Player } from "../gamestate/components";
+import { Component } from "../gamestate/components/base/component";
+import { Entity, EntityID } from "../gamestate/entity";
 import { Server } from "../server";
 import { WebMUDServerPlugin } from "../webmud-server-plugin"
+
+
+export class NPCGreeterComponent extends Component {
+    // "%p" replaced with player name
+    message: string;
+
+    greeted=new Set<EntityID>();
+
+    constructor(message: string) {
+        super();
+        this.message=message;
+    }
+
+}
 
 export class NPCGreeterPlugin extends WebMUDServerPlugin {
     server: Server;
 
     init(server: Server) {
         this.server = server;
-        
+        const self = this;
         // add create-npc-greeter command
         server.commands.addCommand({
             command: 'create-npc-greeter',
@@ -19,14 +37,40 @@ export class NPCGreeterPlugin extends WebMUDServerPlugin {
                 const message = argv.shift();
                 if (!name) return server.error(`Missing value for name`);
                 if (!message) return server.error(`Missing value for message`);
-                this.create(name, message);
+                self.create(name, message);
+                server.info(`Created NPC ${name}`);
             },
         });
     }
 
     create(name: string, message: string) {
         // create entity
+        const e = this.server.gs.createEntity();
+        this.server.gs.entity(e)
+          .add(new Name(name))
+          .add(new HierarchyChild())
+          .add(new HierarchyContainer())
+          .add(new NPCGreeterComponent(message));
 
+        this.server.gs.entity(e).get(HierarchyChild).onMove(() => this.onNPCMove(e));
+        return e;
         // listen to parent changing event
     }
+
+    onNPCMove(e: EntityID) {
+        const parent = this.server.gs.getParent(e);
+        const stop = parent.get(HierarchyContainer).onJoin((target: EntityID) => {
+            if (!(this.server.gs.entity(target).has(Player) && this.server.gs.entity(target).has(ChatChannel))) return;
+            const greeterComponent = this.server.gs.entity(e).get(NPCGreeterComponent);
+            if (greeterComponent.greeted.has(target)) return;
+            greeterComponent.greeted.add(target);
+            this.server.gs.entity(target).get(ChatChannel).event.emit({
+                senderID: e,
+                senderName: this.server.gs.nameOf(e),
+                content: greeterComponent.message.replace("%p", this.server.gs.nameOf(target))
+            });    
+        });
+        this.server.gs.entity(e).get(HierarchyChild).onMove.once(stop);
+    }
+
 }
